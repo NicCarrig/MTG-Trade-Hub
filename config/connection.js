4824ee59706536d192f9b86c0a696396a6e79e93
unknown <clemcarrig90@hotmail.com>
2022-07-08 02:22:46 -0700

    file cleanup

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,8 @@ if(process.env.JAWSDB_URL){
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 }
 else{
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    // sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, 'lem12190', {
             host: 'localhost',
             dialect: 'mysql',
             port: 3306
